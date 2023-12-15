ab621ee4ae4ee2b299fbaef951a46ed04fed1d28
OmerJawaid <omerjawaid0@gmail.com>
2023-12-15 23:10:49 +0500

    mythical creature attack system

--- CP Sem Project/Main.cpp
+++ CP Sem Project/Main.cpp
@@ -1,15 +1,22 @@
 #include<iostream>
 #include<string>
+#include<time.h>
 using namespace std;
 
+void display_character_info(string character_name, string character_class, int character_health, int character_exp);
+void character_creation(string& character_name, string& character_class);
+void battle_system_mythical_creature_attack(int level);
+
+//Main
 int main()
 {
 	string character_name, character_class;
-	int character_health = 100, character_exp=0;
+	int character_health = 100, character_exp=0,level;
 	character_creation(character_name,character_class);
 	display_character_info(character_name,character_class,character_health,character_exp);
 }
-//CHaracter Information
+
+//Character Information
 void display_character_info(string character_name, string character_class, int character_health, int character_exp)
 {
 	cout << "Character Information" << endl
@@ -39,4 +46,15 @@ void character_creation(string& character_name, string& character_class)
 		character_class = "Mage";
 	else if (choice == "c" || choice == "rogue")
 		character_class = "Rogue";
+}
+//Battle System
+void battle_system_mythical_creature_attack(int level)
+{
+	int attack;
+	srand(time(0));
+	for (int i = 0; i < 100; i++)
+	{
+		attack = rand() % level;
+		cout << attack<<endl;
+	}
 }
