6ac93ab8a27027ddc15f63afa18e6f56486b6c5d
OmerJawaid <omerjawaid0@gmail.com>
2023-12-19 21:06:04 +0500

    some fixes

--- CP Sem Project/Main.cpp
+++ CP Sem Project/Main.cpp
@@ -6,7 +6,8 @@ using namespace std;
 void display_character_info(string character_name, string character_class, int character_health, int character_exp);
 void character_creation(string& character_name, string& character_class, int& character_exp , int& level);
 int battle_system_mythical_creature_attack(int attack_level);
-
+void quest();
+//structure for character creation
 struct character {
 	string character_name, character_class;
 	int character_health = 1000, character_exp, level;
@@ -38,23 +39,33 @@ void display_character_info(string character_name, string character_class, int c
 void character_creation(string& character_name, string& character_class, int &character_exp, int &level)
 {
 	string choice;
+	bool flag=true;
 	cout << "Account Creation!" << endl;
 	//Player Name
 	cout << "Enter the player name: ";
 	getline(cin, character_name);
 	//Player Class
-	cout << "Choose the player class " << endl
-		<< "a) Warrior" << endl
-		<< "b) Mage" << endl
-		<< "c) Rogue" << endl
-		<< "Choose";
-	getline(cin, choice);
-	if (choice == "a" || choice == "warrior")
-		character_class = "Warrior";
-	else if (choice == "b" || choice == "mage")
-		character_class = "Mage";
-	else if (choice == "c" || choice == "rogue")
-		character_class = "Rogue";
+	do {
+		cout << "Choose the player class " << endl
+			<< "a) Warrior" << endl
+			<< "b) Mage" << endl
+			<< "c) Rogue" << endl
+			<< "Choose";
+		cin >> choice;
+		choice[0] = tolower(choice[0]);
+		if (choice == "a" || choice == "warrior")
+			character_class = "Warrior";
+		else if (choice == "b" || choice == "mage")
+			character_class = "Mage";
+		else if (choice == "c" || choice == "rogue")
+			character_class = "Rogue";
+		else
+		{
+			cout << "Incorrect" << endl
+				<< "Try Again!";
+			flag = false;
+		}
+	} while (flag == false);
 	character_exp = 0;
 	level = 0;
 }
@@ -66,4 +77,15 @@ int battle_system_mythical_creature_attack(int attack_level)
 		attack = rand() % attack_level;
 		cout << attack<<endl;
 		return attack;
+}
+//Quest Building
+void quest()
+{
+	//Quest Options
+	cout << "Quest: " << endl
+		<<"Beginner"<<endl
+		<<"Intermediate"<<endl
+		<<"Advanced"<<endl
+		<<"Choice: ";
+
 }
