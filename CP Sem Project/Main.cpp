872e43326642e4d74babd6642a7c910e1f77e9cb
OmerJawaid <omerjawaid0@gmail.com>
2023-12-22 16:02:18 +0500

    Till Gate attack

--- CP Sem Project/Main.cpp
+++ CP Sem Project/Main.cpp
@@ -4,21 +4,20 @@
 using namespace std;
 
 void display_character_info(string character_name, string character_class, int character_health, int character_exp);
-void character_creation(string& character_name, string& character_class, int& character_exp , int& level);
+void character_creation();
 int battle_system_mythical_creature_attack(int attack_level);
 void quest();
 //structure for character creation
 struct {
 	string character_name, character_class;
-	int character_health = 1000, character_exp=0, level=0;
+	int character_health = 400, character_exp=0, level=1;
+	string inventory[15];
 }character_1;
 
 //Main
 int main()
 {
-	
 	{
-		
 		char choice;
 		do {
 			//Main Page for start and quit
@@ -38,7 +37,7 @@ int main()
 				char choice;
 				do {
 					//Character creation
-					character_creation(character_1.character_name, character_1.character_class, character_1.character_exp, character_1.level);
+					character_creation();
 					//Character info
 					display_character_info(character_1.character_name, character_1.character_class, character_1.character_health, character_1.character_exp);
 					cout << "Is the above information correct?(Y/N)";
@@ -47,8 +46,41 @@ int main()
 						cout << "Choose th corrent option.";
 						continue;
 				} while (choice=='N'|| choice == 'n');
-				//Contiune the story
 
+				character_1.inventory[0] = "Rope";
+				character_1.inventory[1] = "Health potion";
+
+				//Contiune the story
+				{
+					char entrancechoice;
+					cout << character_1.character_name << " inched closer to the kingdom, blending into the snowy bushes for cover. As he observed the kingdom," << character_1.character_name << "  spotted three potential paths to infiltrate.";
+					//Quest
+					do {
+						cout << "a) The first option caught Jake's eye – the main gate.(Hard)" << endl
+							<< "b) Turning his attention to the second path, Jake discovered a watchtower (Medium)" << endl
+							<< "c) His gaze then shifted to the third option – a tower on the opposite side (Easy)" << endl
+							<< "Choose your path: ";
+						cin >> entrancechoice;
+						entrancechoice=tolower(entrancechoice);
+						if (entrancechoice == 'a')
+						{
+							cout << "He came out of the bushes and moved slowly towards the main gate of the kingdom" << endl
+								<< "Guards: Who are you? Go away otherwise you will have to face the circumstances!" << endl
+								<< character_1.character_name << ": Lets face the circumstances." << endl;
+
+						}
+						else if (entrancechoice == 'b')
+						{
+
+						}
+						else if (entrancechoice == 'c')
+						{
+							
+						}
+						else
+							cout << "Choose correct option.";
+					} while (entrancechoice != 'a' && entrancechoice != 'b' && entrancechoice != 'c');
+				}
 
 
 			}
@@ -70,14 +102,14 @@ void display_character_info(string character_name, string character_class, int c
 		<< "Experience Points: " << character_exp << endl;
 }
 //Character creation
-void character_creation(string& character_name, string& character_class, int &character_exp, int &level)
+void character_creation()
 {
-	string choice;
+	string choice,name;
 	bool flag=true;
 	cout << "\nAccount Creation!" << endl;
 	//Player Name
 	cout << "Enter the player name: ";
-	cin>>character_1.character_name;
+	cin >> character_1.character_name;
 	//Player Class
 	do {
 		cout << "Choose the player class " << endl
@@ -100,8 +132,8 @@ void character_creation(string& character_name, string& character_class, int &ch
 			flag = false;
 		}
 	} while (flag == false);
-	character_exp = 0;
-	level = 0;
+	character_1.character_exp = 0;
+	character_1.level = 0;
 }
 //Battle System
 int battle_system_mythical_creature_attack(int attack_level)
@@ -109,11 +141,46 @@ int battle_system_mythical_creature_attack(int attack_level)
 	int attack;
 	srand(time(0));
 		attack = rand() % attack_level;
-		cout << attack<<endl;
+		cout << attack <<endl;
 		return attack;
 }
+int character_attack(int character_attack_level, int creatures_attack)
+{
+	char attack_choice;
+	do {
+		cout << "Attack Options: " << endl
+			<< "a) Attack" << endl
+			<< "b) Defend" << endl
+			<< "c) Healing" << endl
+			<< "Choice: ";
+		cin >> attack_choice;
+		attack_choice = tolower(attack_choice);
+		if (attack_choice == 'a')
+		{
+			character_1.character_health = character_1.character_health - creatures_attack;
+			int attack;
+			srand(time(0));
+			attack = rand() % character_attack_level;
+			cout <<"Attack: " << attack << endl;
+			return attack;
+		}
+		else if (attack_choice == 'b')
+		{
+			cout << "Defended: " << creatures_attack;
+		}
+		else if (attack_choice == 'c')
+		{
+			character_1.character_health = character_1.character_health + 40;
+			character_1.character_health = character_1.character_health - creatures_attack;
+		}
+		else
+		{
+			cout << "Invalid input ";
+		}
+	}while(attack_choice!='a' && attack_choice != 'b'&& attack_choice != 'c');
+}
 //Quest Building
-void quest()
+void quest1()
 {
 	//Quest Options
 	cout << "Quest: " << endl
