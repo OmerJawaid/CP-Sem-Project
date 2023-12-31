e232a601e4cb2cf62e9b91475e98786b8898d183
OmerJawaid <omerjawaid0@gmail.com>
2023-12-31 23:56:16 +0500

    quest system

--- CP Sem Project/Main.cpp
+++ CP Sem Project/Main.cpp
@@ -29,6 +29,7 @@ struct {
 struct {
 	string name;
 	string difficulty[2];
+	int attack_level[2];
 }quest[16];
 
 void display_character_info();
@@ -36,128 +37,16 @@ void character_creation();
 inline int mythical_creature_attack(int attack_level, int characternumber);
 inline int character_attack(int character_attack_level, int creatures_attack, int creature_number);
 int attack_choice();
-bool battle_system(int creaturenumber, int guard_number);
-string quest_difficulty();
+bool battle_system(int creaturenumber, int guard_number, int attack_number);
+int quest_difficulty();
+void quest_initialization();
+void creature_initialization();
 
 //Main
 int main()
 {
 	string loop_choice;
 	do {
-		//Creatures
-		{
-			{
-				creature[0].name = "Frostbane Guard";
-				creature[0].health = 100;
-				creature[0].attack[0] = 15;
-			}
-			{
-				creature[1].name = "Frostbite Yeti";
-				creature[1].health = 300;
-				creature[1].attack[0] = 50;
-				creature[1].attack[1] = 75;
-				creature[1].attack[2] = 35;
-				creature[1].combo = 150;
-			}
-			{
-				creature[2].name = "Blizzard Golem";
-				creature[2].health = 250;
-				creature[2].attack[0] = 40;
-				creature[2].attack[1] = 65;
-				creature[2].attack[2] = 30;
-				creature[2].combo = 135;
-			}
-			{
-				creature[3].name = "Snow Shriken Ninja";
-				creature[3].health = 350;
-				creature[3].attack[0] = 60;
-				creature[3].attack[1] = 85;
-				creature[3].attack[2] = 45;
-				creature[3].combo = 160;
-			}
-			{
-				creature[4].name = "Glacius Frostend";
-				creature[4].health = 500;
-				creature[4].attack[0] = 60;
-				creature[4].attack[1] = 85;
-				creature[4].attack[2] = 45;
-				creature[4].combo = 300;
-			}
-			{
-				creature[5].name = "King of Mythical Creatures";
-				creature[5].health = 1000;
-				creature[5].attack[0] = 140;
-				creature[5].attack[1] = 180;
-				creature[5].attack[2] = 200;
-				creature[5].combo = 400;
-			}
-		}
-		//Quest
-		{
-			{
-				quest[0].name = "Find a way to Enter the Kingdom";
-				quest[0].difficulty[0] = "Easy";
-				quest[0].difficulty[1] = "Medium";
-				quest[0].difficulty[2] = "Hard";
-			}
-			quest[1].name = "Meet the Informer.";
-			quest[2].name = "Go to the business market for the information and get out.";
-			{
-				quest[3].name = "Find the villagers";
-				quest[3].difficulty[0] = "Eaay";
-				quest[3].difficulty[1] = "Hard";
-			}
-			quest[4].name = "Follow the Informer";
-			{
-				quest[5].name = "Kill Frostbite Yeti";
-				quest[5].difficulty[0] = "Easy";
-				quest[5].difficulty[1] = "Medium";
-				quest[5].difficulty[2] = "Hard";
-			}
-			{
-				quest[6].name = "Kill Bilzzard Golem";
-				quest[6].difficulty[0] = "Easy";
-				quest[6].difficulty[1] = "Medium";
-				quest[6].difficulty[2] = "Hard";
-			}
-			{
-				quest[7].name = "Kill Snow Shuriken Ninja";
-				quest[7].difficulty[0] = "Easy";
-				quest[7].difficulty[1] = "Medium";
-				quest[7].difficulty[2] = "Hard";
-
-			}
-			quest[8].name = "Get out of the Cave.";
-			{
-				quest[9].name = "Kill the Snow Shuriken Ninja";
-				quest[9].difficulty[0] = "Easy";
-				quest[9].difficulty[1] = "Medium";
-				quest[9].difficulty[2] = "Hard";
-			}
-			quest[10].name = "Go to the zipline";
-			{
-				quest[11].name = "Kill The Frostbite Yeti";
-				quest[11].difficulty[0] = "Easy";
-				quest[11].difficulty[1] = "Medium";
-				quest[11].difficulty[2] = "Hard";
-			}
-			{
-				quest[12].name = "Kills the Blizzard Golem";
-				quest[12].difficulty[0] = "Easy";
-				quest[12].difficulty[1] = "Medium";
-				quest[12].difficulty[2] = "Hard";
-			}
-			quest[13].name = "Frozen Vanguard’s plan";
-			quest[14].name = "Selection of inventory.";
-			{
-				quest[15].name = "Kill the Vanguard";
-				quest[15].difficulty[0] = "Easy";
-				quest[15].difficulty[1] = "Medium";
-				quest[15].difficulty[2] = "Hard";
-			}
-			quest[16].name = "-The final battle with king, giants forestbane and mythical king story ";
-			//Reamianing
-		}
 		{
 			string choice;
 			do {
@@ -169,6 +58,8 @@ int main()
 				cin >> choice;
 				if (choice == "a")
 				{
+					quest_initialization();
+					creature_initialization();
 					//Storyline
 					cout << "On a calm winter day, Eldoria Kingdom got hit by a surprise snowstorm. Everything turned white as snow covered the kingdom. But then, weird creatures from the nearby jungle showed up and attacked Eldoria. They took the people hostage and took control of the kingdom." << endl;
 					system("pause");
@@ -214,6 +105,7 @@ int main()
 										<< character.name << ": Lets face the circumstances." << endl;
 									system("pause");
 									//Battle with all 5 guards
+
 									for (int i = 1; i <= 5; i++)
 									{
 										bool flag = false;
@@ -868,8 +760,8 @@ inline int character_attack(int character_attack_level, int creatures_attack, in
 		}
 	} while (attack_choice != "a" && attack_choice != "b" && attack_choice != "c");
 }
-
-bool battle_system(int creaturenumber,int guard_number)
+//Main battle function
+bool battle_system(int creaturenumber,int guard_number, int attack_number)
 {	
 	creature[0].health = 100;
 	cout << creature[creaturenumber].name << endl;
@@ -898,34 +790,152 @@ bool battle_system(int creaturenumber,int guard_number)
 		return flag;
 }
 
-string quest_difficulty()
+int quest_difficulty()
 {
-	int choice;
+	int choice,attack_level;
 	string difficulty_level;
 	do {
 		cout << "How hard would you like to fight: " << endl
-			<< "\t1) Easy" << endl
-			<< "\t2) Midium" << endl
-			<< "\t3) Hard" << endl
+			<< "\t1) " << quest->difficulty[0] << endl
+			<< "\t2) " << quest->difficulty[1]<< endl
+			<< "\t3) " << quest->difficulty[2] << endl
 			<< "Enter your choice: " << endl;
 		cin >> choice;
 			if (choice != 1 && choice != 2 && choice != 3)
 				cout << "You Entered Wrong Input. Try Again!"<<endl;
 	} while (choice != 1 && choice != 2 && choice != 3);
+	choice--;
 	switch (choice)
 	{
 		case 1:
 		{
-			difficulty_level = "Easy";
+			return choice;
 		}
 		case 2:
 		{
-			difficulty_level = "Mediun";
+			return choice;
 		}
 		case 3:
 		{
-			difficulty_level = "Easy";
+			return choice;
 		}
 	}
-	return difficulty_level;
+}
+
+void quest_initialization()
+{
+	{
+		quest[0].name = "Find a way to Enter the Kingdom";
+		quest[0].difficulty[0] = "Easy";
+		quest[0].difficulty[1] = "Medium";
+		quest[0].difficulty[2] = "Hard";
+	}
+	quest[1].name = "Meet the Informer.";
+	quest[2].name = "Go to the business market for the information and get out.";
+	{
+		quest[3].name = "Find the villagers";
+		quest[3].difficulty[0] = "Eaay";
+		quest[3].difficulty[1] = "Hard";
+	}
+	quest[4].name = "Follow the Informer";
+	{
+		quest[5].name = "Kill Frostbite Yeti";
+		quest[5].difficulty[0] = "Easy";
+		quest[5].difficulty[1] = "Medium";
+		quest[5].difficulty[2] = "Hard";
+	}
+	{
+		quest[6].name = "Kill Bilzzard Golem";
+		quest[6].difficulty[0] = "Easy";
+		quest[6].difficulty[1] = "Medium";
+		quest[6].difficulty[2] = "Hard";
+	}
+	{
+		quest[7].name = "Kill Snow Shuriken Ninja";
+		quest[7].difficulty[0] = "Easy";
+		quest[7].difficulty[1] = "Medium";
+		quest[7].difficulty[2] = "Hard";
+
+	}
+	quest[8].name = "Get out of the Cave.";
+	{
+		quest[9].name = "Kill the Snow Shuriken Ninja";
+		quest[9].difficulty[0] = "Easy";
+		quest[9].difficulty[1] = "Medium";
+		quest[9].difficulty[2] = "Hard";
+	}
+	quest[10].name = "Go to the zipline";
+	{
+		quest[11].name = "Kill The Frostbite Yeti";
+		quest[11].difficulty[0] = "Easy";
+		quest[11].difficulty[1] = "Medium";
+		quest[11].difficulty[2] = "Hard";
+	}
+	{
+		quest[12].name = "Kills the Blizzard Golem";
+		quest[12].difficulty[0] = "Easy";
+		quest[12].difficulty[1] = "Medium";
+		quest[12].difficulty[2] = "Hard";
+	}
+	quest[13].name = "Frozen Vanguard’s plan";
+	quest[14].name = "Selection of inventory.";
+	{
+		quest[15].name = "Kill the Vanguard";
+		quest[15].difficulty[0] = "Easy";
+		quest[15].difficulty[1] = "Medium";
+		quest[15].difficulty[2] = "Hard";
+	}
+	quest[16].name = "-The final battle with king, giants forestbane and mythical king story ";
+	//Remaining
+}
+
+void creature_initialization()
+{
+	{
+		creature[0].name = "Frostbane Guard";
+		creature[0].health = 100;
+		creature[0].attack[0] = 15;
+		creature[0].attack[1] = 20;
+		creature[0].attack[2] = 25;
+	}
+	{
+		creature[1].name = "Frostbite Yeti";
+		creature[1].health = 300;
+		creature[1].attack[0] = 50;
+		creature[1].attack[1] = 75;
+		creature[1].attack[2] = 35;
+		creature[1].combo = 150;
+	}
+	{
+		creature[2].name = "Blizzard Golem";
+		creature[2].health = 250;
+		creature[2].attack[0] = 40;
+		creature[2].attack[1] = 65;
+		creature[2].attack[2] = 30;
+		creature[2].combo = 135;
+	}
+	{
+		creature[3].name = "Snow Shriken Ninja";
+		creature[3].health = 350;
+		creature[3].attack[0] = 60;
+		creature[3].attack[1] = 85;
+		creature[3].attack[2] = 45;
+		creature[3].combo = 160;
+	}
+	{
+		creature[4].name = "Glacius Frostend";
+		creature[4].health = 500;
+		creature[4].attack[0] = 60;
+		creature[4].attack[1] = 85;
+		creature[4].attack[2] = 45;
+		creature[4].combo = 300;
+	}
+	{
+		creature[5].name = "King of Mythical Creatures";
+		creature[5].health = 1000;
+		creature[5].attack[0] = 140;
+		creature[5].attack[1] = 180;
+		creature[5].attack[2] = 200;
+		creature[5].combo = 400;
+	}
 }
