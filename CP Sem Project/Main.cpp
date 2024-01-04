4b86938fd8fd67c8c836786d6a74c7476a857b74
ahsansheikh14 <ahsansheikh321123@gmail.com>
2024-01-04 13:16:48 +0500

    final

--- CP Sem Project/Main.cpp
+++ CP Sem Project/Main.cpp
@@ -898,6 +898,7 @@ int main()
 
 						}
 						cout << RED_TEXT << "Chapter 5 End" << RESET_COLOR << endl;
+						cout << GREEN_TEXT << "GAME END!" << RESET_COLOR << endl;
 						system("cls");
 						//Chapter 5 Ends
 				}
